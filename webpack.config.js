1bc79a42c71fb57cc3099f4518382cc2a87ff331
Vladislav <vladovinkin@gmai.com>
2020-09-30 17:26:38 +0300

    Finish css layout

--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,8 @@ module.exports = {
   devServer: {
     port: 3000,
     hot: isDev,
+    contentBase: path.resolve(__dirname, 'src'),
+    watchContentBase: true,
   },
   plugins: [
     new CleanWebpackPlugin(),
